be971cdeb87fb9c0477d54f8fadad089649c66d2
Curtisdak <curtis.dakouri@gmail.com>
2025-06-14 22:23:59 +0200

    trial07

--- src/app/error/page.tsx
+++ src/app/error/page.tsx
@@ -1,32 +1,14 @@
-"use client"
+"use client";
 
-import { useRouter,useSearchParams } from "next/navigation";
-import { Button } from "@/components/ui/button";
-import { AlertTriangle, Home } from "lucide-react";
-import Link from "next/link";
 import { Suspense } from "react";
+import { FullLoading } from "@/components/customComponents/FullLoading"; // optional
+
+import ErrorMessageContent from "./ErrorMessageContent"; // create this
 
 export default function ErrorPage() {
-  const router = useRouter();
-  const searchParams = useSearchParams()
-  const errorMessage = searchParams.get('message')  ;
-
-  const handleBack = () => {
-    router.back(); // Revenir à la page précédente
-  };
-
   return (
-    
-     <Suspense fallback={<div>Chargement...</div>}>
-    <div className="w-screen h-screen flex flex-col items-center justify-center gap-6 bg-background">
-      <AlertTriangle className="w-16 h-16 text-destructive animate-ping" />
-      <h1 className="text-3xl font-bold text-destructive">Oups !</h1>
-      <p className="text-muted-foreground text-center p-2 text-2xl">{errorMessage}</p>
-      <Button onClick={handleBack} className="mt-4 animate-pulse text-white ">
-        Revenir à la page précédente
-      </Button>
-      <Link href={'/'} className="bg-primary p-4 rounded-full mt-10 hover:animate-pulse hover:bg-secondary " ><Home size={40} /></Link>
-    </div>
+    <Suspense fallback={<FullLoading />}>
+      <ErrorMessageContent />
     </Suspense>
   );
 }
